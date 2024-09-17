c4f40961568021fd72e0615eddb8c8c70ec750b3
me3za <medkrix1@gmail.com>
2024-09-17 07:11:14 +0100

    prisma config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
+declare global {
+	var prisma: PrismaClient | undefined
+  }
+  
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {

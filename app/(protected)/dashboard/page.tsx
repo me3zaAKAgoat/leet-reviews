f3832b0f8f898b8981c08f1f858d63b02230c83f
me3za <53712804+me3zaAKAgoat@users.noreply.github.com>
2024-09-22 12:30:10 +0100

    review card styling

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,6 +1,19 @@
+import ReviewCard from "@/components/ReviewCard";
 import { Input } from "@/components/ui/input";
 
 export default function Layout() {
+  const Review = {
+    company: {
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMjDUu5Cugm9VpGLWK5FN-c6zwi9Y1x8DZGA&s",
+      name: "1337",
+      location: "Khouribga",
+    },
+    review: {
+      rating: 5,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id.",
+	  createdAt: "2021-09-01",
+	},
+  };
   return (
     <>
       <main className="flex grow p-4">
@@ -9,11 +22,7 @@ export default function Layout() {
         </div>
         <div className="w-full grow">
           <ul className="flex flex-col gap-4 px-4">
-            <li className="bg-stone-400 p-4">Item 1</li>
-            <li className="bg-stone-400 p-4">Item 2</li>
-            <li className="bg-stone-400 p-4">Item 3</li>
-            <li className="bg-stone-400 p-4">Item 4</li>
-            <li className="bg-stone-400 p-4">Item 5</li>
+            <ReviewCard Review={Review} />
           </ul>
         </div>
       </main>
--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -1,3 +1,35 @@
-export default function ReviewCard() {
-  return <></>;
+"use client";
+import Image from "next/image";
+import { Rating } from "react-simple-star-rating";
+import { Button } from "@/components/ui/button";
+
+export default function ReviewCard({ Review }: any) {
+  return (
+    <div className="flex flex-col sm:flex-row gap-4 p-4 border border-stone-500 rounded-sm">
+      <div className="flex sm:flex-col gap-4">
+        <img
+          src={Review.company.image}
+          width={80}
+          height={80}
+          alt={Review.company.name}
+        />
+        <div>
+          <h1>{Review.company.name}</h1>
+          <h2>{Review.company.location}</h2>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <Rating
+          SVGstyle={{ display: "inline", height: "15px", width: "15px" }}
+          readonly
+          initialValue={Review.review.rating}
+        />
+        <p>{Review.review.description}</p>
+        <div className="flex items-end justify-between">
+          <p className="text-xs">created at: {Review.review.createdAt}</p>
+          <Button>Expand</Button>
+        </div>
+      </div>
+    </div>
+  );
 }

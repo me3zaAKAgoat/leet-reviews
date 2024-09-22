e77b320e653c53da28581a5df735ffc1d57de06b
me3za <53712804+me3zaAKAgoat@users.noreply.github.com>
2024-09-22 10:04:40 +0100

    added dropdown menu on avatar

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -5,6 +5,14 @@ import Image from "next/image";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { AvatarFallback } from "@radix-ui/react-avatar";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default async function Layout({
   children,
@@ -14,7 +22,6 @@ export default async function Layout({
   const session = await auth();
 
   if (!session) redirect("/signin");
-  console.log(session);
 
   return (
     <>
@@ -24,13 +31,24 @@ export default async function Layout({
         </Link>
         <div className="flex items-center gap-4">
           <Button className="font-bold">Add A Review</Button>
-          <Avatar>
-            <AvatarImage
-              src={session.user?.image ?? undefined}
-              alt={session.user?.name ?? undefined}
-            />
-            <AvatarFallback>{session.user?.name}</AvatarFallback>
-          </Avatar>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar>
+                <AvatarImage
+                  src={session.user?.image ?? undefined}
+                  alt={session.user?.name ?? undefined}
+                />
+                <AvatarFallback>{session.user?.name}</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Sign Out</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </header>
       {children}
